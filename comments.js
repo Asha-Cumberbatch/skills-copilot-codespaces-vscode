e8bdac4e26fa0148a6b15dc76209bf371274d17e
ACCyNatGlobal <asha.cumberbatch@cynatglobal.com>
2025-02-07 20:11:14 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// Create web server
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<html><body><h1>Hello, World!</h1></body></html>")
+
+def run(server_class=HTTPServer, handler_class=MyHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
